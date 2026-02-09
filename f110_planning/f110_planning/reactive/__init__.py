4faab77bdf125994e93d6af1b1389b3b5d7cfe5c
Prateek Ganguli <prateek.ganguli@gmail.com>
2026-02-09 16:59:03 -0500

    Implement dynamic waypoint based planner

--- f110_planning/f110_planning/reactive/__init__.py
+++ f110_planning/f110_planning/reactive/__init__.py
@@ -1,5 +1,6 @@
 from .bubble_planner import BubblePlanner
 from .disparity_extender_planner import DisparityExtenderPlanner
+from .dynamic_waypoint_planner import DynamicWaypointPlanner
 from .gap_follower_planner import GapFollowerPlanner
 from .longest_path_planner import LongestPathPlanner
 from .longest_reachable_path_planner import LongestReachablePathPlanner
@@ -10,6 +11,7 @@ from .wall_following_distance_planner import WallFollowingDistancePlanner
 __all__ = [
     "BubblePlanner",
     "DisparityExtenderPlanner",
+    "DynamicWaypointPlanner",
     "GapFollowerPlanner",
     "LongestPathPlanner",
     "LongestReachablePathPlanner",
--- f110_planning/f110_planning/render_callbacks/__init__.py
+++ f110_planning/f110_planning/render_callbacks/__init__.py
@@ -1,4 +1,5 @@
 from .camera import camera_tracking
+from .dynamic_waypoint import create_dynamic_waypoint_renderer
 from .lidar import create_heading_error_renderer, render_lidar, render_side_distances
 from .ransac_walls import create_ransac_walls_renderer
 from .trace import create_trace_renderer
@@ -9,6 +10,7 @@ __all__ = [
     "render_lidar",
     "render_side_distances",
     "create_heading_error_renderer",
+    "create_dynamic_waypoint_renderer",
     "create_ransac_walls_renderer",
     "create_waypoint_renderer",
     "create_trace_renderer",
